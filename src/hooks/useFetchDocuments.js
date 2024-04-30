e3cce0b0b8a7f6f2f5dfa6123842b847b3c445a7
elissandroa <elissandroa@hotmail.com>
2024-04-30 11:29:08 -0300

    Dashboard

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -1,63 +1,69 @@
-import { useEffect, useState } from 'react';
-import { db } from '../firebase/config';
+import { useEffect, useState } from "react";
+import { db } from "../firebase/config";
 
 import {
-    collection,
-    query,
-    orderBy,
-    onSnapshot,
-    where,
-    QuerySnapshot
-} from 'firebase/firestore';
-
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
 
 export const useFetchDocuments = (docCollection, search, uid) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [cancelled, setCancelled] = useState(false);
 
-    const [documents, setDocuments] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(null);
-    const [cancelled, setCancelled] = useState(false);
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
 
-    useEffect(() => {
+      setLoading(true);
 
-        async function loadData() {
-            if (cancelled) return;
+      const collectionRef = await collection(db, docCollection);
 
-            setLoading(true);
+      try {
+        let q;
 
-            const collectionRef = await collection(db, docCollection);
-
-            try {
-                let q;
-
-                if (search) {
-                    q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"));
-                } else {
-                    q = await query(collectionRef, orderBy('createdAt', 'desc'));
-                }
-
-                await onSnapshot(q, (QuerySnapshot) => {
-                    setDocuments(
-                        QuerySnapshot.docs.map((doc) => ({
-                            id: doc.id,
-                            ...doc.data(),
-                        }))
-                    )
-                })
-                setLoading(false);
-            } catch (error) {
-                console.log(error);
-                setError(error.message);
-
-                setLoading(false);
-            }
+        if (search) {
+          q = await query(
+            collectionRef,
+            where("tagsArray", "array-contains", search),
+            orderBy("createdAt", "desc")
+          );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt", "desc")
+          );
+        } else {
+          q = await query(collectionRef, orderBy("createdAt", "desc"));
         }
-        loadData();
-    }, [docCollection, search, uid, cancelled]);
 
-    useEffect(() => {
-        return () => setCancelled(true);
-    }, [])
+        await onSnapshot(q, (QuerySnapshot) => {
+          setDocuments(
+            QuerySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
 
-    return {documents, loading, error};
-}
+        setLoading(false);
+      }
+    }
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+};
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,50 @@
-import './styles.css';
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 export const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {
+
+  }
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div><h1>Dashboard</h1></div>
-  )
-}
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className="noposts">
+          <p>Não foram encontrados posts</p>
+          <Link className="btn" to={"/posts/create"}>
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div className="post_header">
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {posts && posts.map((post) => <div key={post.id} className="post_row">
+            <p>{post.title}</p>
+            <div>
+              <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+              <Link to={`/posts/edit${post.id}`} className="btn btn-outline">Edit</Link>
+              <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
+            </div>
+          </div>)}
+        </>
+      )}
+    </div>
+  );
+};
